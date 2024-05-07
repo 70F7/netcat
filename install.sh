1c4c2ac7a8c5daa4b200ffe4aebe25ac7d5033a3
70F7 <102881921+70F7@users.noreply.github.com>
2024-05-07 13:07:33 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Installing Moduels"
+
+sudo apt install netcat-traditional
+sudo apt install git 
+
+git clone https://github.com/70F7/netcat.git
+
+chmod 775 myscript.sh
+
+mv myscript.sh /etc/init.d

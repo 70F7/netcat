e6e1afb6100c58f1ff86c06db725e06e0e121615
70F7 <102881921+70F7@users.noreply.github.com>
2024-05-07 13:45:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 echo "Installing Moduels"
-
 sudo apt install netcat-traditional
-
 chmod 775 myscript
 sleep 5
 mv myscript /etc/init.d
-
-echo "Rebooting in 10"
-echo "9"
-echo "8"
+sleep 1
+init u

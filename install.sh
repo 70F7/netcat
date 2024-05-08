6373e062f5fe6f60709932d024ab83d20ce81b16
70F7 <102881921+70F7@users.noreply.github.com>
2024-05-08 13:24:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,13 @@
 
 echo "Installing Moduels"
 sudo apt install netcat-traditional
-sudo apt install ncat
 chmod 775 myscript
 sleep 5
 mv myscript /etc/init.d
+sleep 3
+crontab -l > crontab_new 
+echo "@reboot systemctl start myscript.service" >> crontab_new
+crontab crontab_new
+rm crontab_new
 sleep 1
 init u
